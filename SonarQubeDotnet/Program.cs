356be257b697ddaeb4cdfb10dc8507ecc1a3e93e
Caique Augusto Ferreira <caiqueaugustoferreira@gmail.com>
2024-11-12 22:07:40 -0300

    ....

--- SonarQubeDotnet/Program.cs
+++ SonarQubeDotnet/Program.cs
@@ -1,8 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ComplexCodeExample;
+﻿using System;
+using System.Collections;
 
-Console.WriteLine("Hello, World!");
+namespace CalculadoraRuim
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a operação desejada: (+, -, *, /, %, média, mediana)");
+            string operacao = Console.ReadLine();
 
-BankAccount a = new(1000, "123");
+            Console.WriteLine("Digite o primeiro número:");
+            double num1 = double.Parse(Console.ReadLine());
 
-// TODO: SASA
+            Console.WriteLine("Digite o segundo número (se aplicável):");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double resultado = 0;
+
+            if (operacao == "+")
+            {
+                resultado = num1 + num2;
+            }
+            else if (operacao == "-")
+            {
+                resultado = num1 - num2;
+            }
+            else if (operacao == "*")
+            {
+                resultado = num1 * num2;
+            }
+            else if (operacao == "/")
+            {
+                resultado = num1 / num2;
+            }
+            else if (operacao == "%")
+            {
+                resultado = num1 * (num2 / 100);
+            }
+            else if (operacao == "média")
+            {
+                resultado = (num1 + num2) / 2;
+            }
+            else if (operacao == "mediana")
+            {
+                ArrayList lista = new ArrayList { num1, num2 };
+                lista.Sort();
+                resultado = (double)lista[lista.Count / 2];
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida!");
+            }
+
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    }
+}
